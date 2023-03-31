c0029a6b79db5ba864ae9cb0e0ef9bf075cf5f26
Johan Contreras <j.contreras.granados.04@gmail.com>
2023-03-30 23:55:49 -0500

    Refactor en el codigo:  - Refactor en: model.py - prueba.py

--- prueba.py
+++ prueba.py
@@ -3,7 +3,7 @@ from entity import *
 
 
 class Prueba:
-    produ = Producto("P009", "Armario", 'Hogar', 12.0, 3)
+    produ = Producto('P009', 'Armario', 'Hogar', 12.0, 3)
     controller = ProductoController()
     message = controller.procesarProducto(produ, ADD)
     print(message)
